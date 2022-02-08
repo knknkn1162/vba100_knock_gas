e007a462eaaf9d1a7658770bf2e8a0a80bdd2f68
knknkn1162 <knknkn1162@gmail.com>
2022-02-08 23:54:09 +0900

    generalize; impl intersect

--- src/ex003/main.ts
+++ src/ex003/main.ts
@@ -1,5 +1,11 @@
+import Sheet = GoogleAppsScript.Spreadsheet.Sheet;
+import Range = GoogleAppsScript.Spreadsheet.Range;
+function intersect(sht: Sheet, rows: number, columns: number): Range {
+    const rng = sht.getDataRange();
+    return rng.offset(rows, columns, rng.getNumRows()-rows, rng.getNumRows()-columns)
+}
+
 function main(): void {
-    const sht = SpreadsheetApp.getActiveSheet()
-    Logger.log(sht.getDataRange().getA1Notation());
-    sht.getDataRange().offset(1,1).clearContent()
+    const sht = SpreadsheetApp.getActiveSheet();
+    intersect(sht, 1, 1).clearContent();
 }
