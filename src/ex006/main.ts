f14e7d2c006166adc9d02b7e5cce50889a0ed88a
knknkn1162 <knknkn1162@gmail.com>
2022-02-11 09:31:44 +0900

    use slice

--- src/ex006/main.ts
+++ src/ex006/main.ts
@@ -3,7 +3,7 @@ function main(): void {
     const rng = sht.getDataRange();
     const mat = rng.getValues();
     Array.from(Array(mat.length).keys())
-        .filter(val => val !== 0)
+        .slice(1)
         .filter(val => !(""+mat[val][0]).match(/.+-.+/))
         .forEach(val => mat[val][3] = "=RC[-2]*RC[-1]")
     rng.setValues(mat);
