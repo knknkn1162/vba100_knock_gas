79f16da674f6ed5229234182e84e27efa6fe83c2
knknkn1162 <knknkn1162@gmail.com>
2022-02-15 11:16:42 +0900

    impl ex014

---
+++ src/ex014/main.ts
@@ -0,0 +1,16 @@
+import Sheet = GoogleAppsScript.Spreadsheet.Sheet;
+
+function main(): void {
+    const book = SpreadsheetApp.getActiveSpreadsheet();
+    const shts = book.getSheets()
+        .filter(sht => !sht.getName().includes("社外秘"));
+    for(const sht of shts) {
+        const mat = sht.getDataRange().getValues();
+        sht.getDataRange().setValues(mat);
+        sht.showSheet();
+        sht.getRange("A1").activate();
+    }
+    book.getSheets()
+        .filter(sht => sht.getName().includes("社外秘"))
+        .forEach(sht => book.deleteSheet(sht));
+}
