70c82dea4ad5289f32b7261dc952359b3ec1ef4c
knknkn1162 <knknkn1162@gmail.com>
2022-02-16 15:13:18 +0900

    improve main.ts; introduce generator function

--- src/ex020/main.ts
+++ src/ex020/main.ts
@@ -1,10 +1,44 @@
-import Sheet = GoogleAppsScript.Spreadsheet.Sheet;
+import FileIterator = GoogleAppsScript.Drive.FileIterator;
+import File = GoogleAppsScript.Drive.File;
+import Folder = GoogleAppsScript.Drive.Folder;
+import FolderIterator = GoogleAppsScript.Drive.FolderIterator;
+type DriveItem = File | Folder;
+type GASIterator = FileIterator | FolderIterator;
+
+function* gen(iter: GASIterator) {
+    while(iter.hasNext()) {
+      yield iter.next();
+    }
+}
+
+function createBackupFolder(str: string, parent: Folder): Folder {
+    const root = DriveApp.getFoldersByName("spreadsheet").next();
+    const iter = gen(root.getFoldersByName(str));
+    const arr = Array.from(iter)
+    const isFolder = (obj: DriveItem): obj is Folder => "createFolder" in obj;
+    switch(arr.length) {
+        case 2:
+            console.error(`${str} folders has at least two`);
+            break;
+        case 1:
+            Logger.log(`exist folder: ${str}; do nothing`)
+            if(isFolder(arr[0])) return arr[0];
+        case 0:
+            return root.createFolder(str)
+        default:
+            console.error("unkown error");
+            break;
+    }
+    return;
+}
 
 function main(): void {
     const book = SpreadsheetApp.getActiveSpreadsheet();
     const datestr = Utilities.formatDate(new Date(), "JST", "yyyyMMddhhmmss");
-    Logger.log(datestr);
-    //const newSS = SpreadsheetApp.create(`${book.getName()}_${datestr}`);
-
-    Logger.log(DriveApp.getRootFolder().getName())
+    const root = DriveApp.getFoldersByName("spreadsheet").next();
+    const dir = createBackupFolder(`BACKUP_${book.getName()}`, root);
+    const fname = `${book.getName()}_${datestr}`;
+    book.copy(fname);
+    // The Copied file creates just in `DriveApp.getRootFolder()`, so we must move to `dir`
+    DriveApp.getFilesByName(fname).next().moveTo(dir);
 }
